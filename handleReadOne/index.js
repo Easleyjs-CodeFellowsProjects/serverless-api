fea59dc3f7e95ad02a06dfce9503d22c888d640f
Josh Easley <easleyjs@gmail.com>
2023-10-13 21:11:28 -0700

    Adds working readOne handler

---
+++ handleReadOne/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const dynamoose = require('dynamoose');
+
+const comicsSchema = new dynamoose.Schema({
+  id: Number,
+  title: String,
+  value: Number,
+  rarity: String,
+});
+
+// create our 'Model'
+const comicsModel = dynamoose.model('comics', comicsSchema);
+
+exports.handler = async (event) => {
+
+  let results = await comicsModel.query('id').eq(parseInt(event.pathParameters.id)).exec();
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(results),
+  };
+  return response;
+};
